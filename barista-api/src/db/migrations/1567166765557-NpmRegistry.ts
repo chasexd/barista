afd0815623329bfe373ebfd3ddb511ef9b4c45d8
Randy Olinger <randy.olinger@optum.com>
2020-05-28 13:13:31 -0500

    updates to system config tables.

--- barista-api/src/db/migrations/1567166765557-NpmRegistry.ts
+++ barista-api/src/db/migrations/1567166765557-NpmRegistry.ts
@@ -9,5 +9,7 @@ export class NpmRegistry1567166765557 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(`ALTER TABLE "system_configuration" ADD "npm_cache_directory" character varying`);
     await queryRunner.query(`ALTER TABLE "system_configuration" ADD "npm_registry" character varying`);
+    await queryRunner.query(`UPDATE "system_configuration" SET "npm_registry"='https://registry.npmjs.org/'`);
+    await queryRunner.query(`UPDATE "system_configuration" SET "npm_cache_directory"='/usr/src/app/tools'`);
   }
 }
